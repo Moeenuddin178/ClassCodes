0f8c4072f34884e13623e6e6b9d191693aa55f45
Moeenuddin <34792583+Moeenuddin178@users.noreply.github.com>
2022-07-21 21:26:01 +0500

    Initial commit

---
+++ app/src/main/java/my/project/a2appuidesign/class3.java
@@ -0,0 +1,22 @@
+package my.project.a2appuidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class class3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class3);
+    }
+
+
+    public void click(View view) {
+        startActivity(new Intent(class3.this,MainActivity.class));
+    }
+}
