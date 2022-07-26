7f562de77dd1dc6b1292071b0bb5a55f21d687bd
Moeenuddin <34792583+Moeenuddin178@users.noreply.github.com>
2022-07-26 14:12:05 +0500

    Class4

---
+++ app/src/main/java/my/project/a2appuidesign/class5.java
@@ -0,0 +1,14 @@
+package my.project.a2appuidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class class5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class5);
+    }
+}
